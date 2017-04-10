9f7f249b1277cf44b527a295d954dfbc42bd99ba
Saule BN <beyonce@thinkful.com>
2017-04-10 15:04:48 -0400

    AddedAppState

--- quiz.js
+++ quiz.js
@@ -1,2 +1,59 @@
-console.log('hello');
+const appState = { 
+	questions: [
+	{questionText:'What is 1 + 3', answerChoice: [7,5,4,1,6],correctAnswer: 4},
+	{questionText:'What is 2 + 3', answerChoice: [7,5,4,1,6],correctAnswer: 5},
+	{questionText:'What is 3 + 3', answerChoice: [7,5,4,1,6],correctAnswer: 6},
+	{questionText:'What is 4 + 3', answerChoice: [7,5,4,1,6],correctAnswer: 7}],
 
+	currentQuestion: 0,
+	score: 0,
+	correctAnswer: 'You are a Math genius!',
+	incorrectAnswer: 'Try again!'
+}
+
+
+
+
+
+	
+
+
+
+
+//Submit button Function
+
+
+
+
+
+
+
+//Submit Answer choice function/event
+
+
+
+
+
+//Next button function
+
+
+
+
+
+//Validate questions function- correct/incorrect/score incrementer(if/else)
+
+
+
+
+
+
+//Start new quizz button function
+
+
+
+
+
+
+
+
+//Shouw result score string
